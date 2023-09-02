611a66a3cf0a1c411d608ae85adc84dfbcc4aee0
carlosehmke <carlos.ehmke.agra@gmail.com>
2023-09-02 12:10:09 -0300

    Commit initial

---
+++ Aula1.py
@@ -0,0 +1,11 @@
+"""
+Esse é um docstring, porém o interpretador lê. 
+Ou seja, não é um comentário. Ele guarda essa informação na memória.
+
+"""
+
+# Essa é a primeira aula de Python
+
+print('Hello world"')
+print('Hello world"')
+print('Hello world"')
---
+++ Teste/Aula2.py
@@ -0,0 +1,3 @@
+
+
+print('Argu \n mento', 'teste', sep='xxx')
