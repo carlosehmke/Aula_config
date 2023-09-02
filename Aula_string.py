fa908ea2bdd06f70462433e0d83a0a77eddb7b51
carlosehmke <carlos.ehmke.agra@gmail.com>
2023-09-02 12:25:16 -0300

    Adicionado a aula de strings

---
+++ Aula_string.py
@@ -0,0 +1,16 @@
+# Ptyhon é considerado uma linguagem de programação Dinâmica e Forte.
+
+
+print('123+123')
+
+print(123+123)
+
+print("Ele falou \"Olá mundo\" outro dia") # Escape \ é usado para informar ao interpretador que o próximo caractere deve ser ignorado (não interpretado)
+
+print(r"Ele falou \"Olá mundo\" outro dia") # O caractere "r" antes da string fará com que o escape "\" seja mostrado
+
+
+print(r"Ele falou \"Olá mundo\" outro dia")
+
+
+print('Ele falou \"Olá mundo\" outro dia')
